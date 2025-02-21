b2f765c9e79512bfe5b40fc13749f746c242f76a
Drizzle210 <anhduc5a1tp@gmail.com>
2025-02-21 16:49:39 +0700

    init receiving cw20 contract

---
+++ receiving-cw20-tokens/src/contract.rs
@@ -0,0 +1,104 @@
+#[cfg(not(feature = "library"))]
+use cosmwasm_std::entry_point;
+use cosmwasm_std::{Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult, Addr};
+// use cw2::set_contract_version;
+
+use crate::error::ContractError;
+use crate::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+use crate::state::*;
+
+/*
+// version info for migration info
+const CONTRACT_NAME: &str = "crates.io:receiving-cw20-tokens";
+const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
+*/
+
+// store admin and whitelist token (symbol & token contract address) to blockchain
+#[cfg_attr(not(feature = "library"), entry_point)]
+pub fn instantiate(
+    deps: DepsMut,
+    _env: Env,
+    info: MessageInfo,
+    msg: InstantiateMsg,
+) -> Result<Response, ContractError> {
+    
+    let admin = info.sender.to_string();
+    let cw20_whitelist: Vec<(String, Addr)> = vec![(
+        msg.token_symbol,
+        msg.token_contract_address,
+    )];
+
+    CONFIG.save(
+        deps.storage,
+        &Config {
+            admin: deps.api.addr_validate(&admin).unwrap(),
+            cw20_wl: cw20_whitelist,
+        }
+    )?;
+
+    Ok(Response::new()
+    .add_attribute("method", "instantiate")
+    .add_attribute("admin", admin))
+}
+
+#[cfg_attr(not(feature = "library"), entry_point)]
+pub fn execute(
+    deps: DepsMut,
+    env: Env,
+    info: MessageInfo,
+    msg: ExecuteMsg,
+) -> Result<Response, ContractError> {
+    match msg {
+        ExecuteMsg::Receive(receive_msg) => execute::execute_receive(deps, env, info, receive_msg),
+    }
+}
+
+pub mod execute {
+    use cosmwasm_std::from_json;
+    use super::*;
+    use cw20::{Cw20ReceiveMsg, Cw20CoinVerified, Balance};
+
+    use crate::msg::ReceiveMsg;
+
+    pub fn execute_receive(
+        deps: DepsMut,
+        env: Env,
+        info: MessageInfo,
+        wrapper: Cw20ReceiveMsg,
+    ) -> Result<Response, ContractError> {
+        
+        let msg: ReceiveMsg = from_json(&wrapper.msg).unwrap(); // msg included in Send {contract, amount, **msg**} execute on the cw20 contract
+        let user_wallet = deps.api.addr_validate(&wrapper.sender).unwrap();
+
+        let balance = Balance::Cw20(Cw20CoinVerified {
+            address: info.sender.clone(), // cw20 contract this message was sent from
+            amount: wrapper.amount,
+        });
+
+        let config = CONFIG.load(deps.storage)?;
+        // @todo: check if the token is in the whitelist
+
+        match msg {
+            ReceiveMsg::AnExecuteMsg {} => {
+                execute_do_somthing(deps, &user_wallet, &info.sender, balance)
+            }
+        }
+    }
+
+    pub fn execute_do_somthing(
+        _deps: DepsMut,
+        _user_wallet: &Addr,
+        _cw20_contract_addr: &Addr,
+        _balance: Balance,
+    ) -> Result<Response, ContractError> {
+        Ok(Response::default())
+    }
+}
+
+#[cfg_attr(not(feature = "library"), entry_point)]
+pub fn query(_deps: Deps, _env: Env, _msg: QueryMsg) -> StdResult<Binary> {
+    unimplemented!()
+}
+
+#[cfg(test)]
+mod tests {}
---
+++ receiving-cw20-tokens/src/msg.rs
@@ -0,0 +1,23 @@
+use cosmwasm_schema::{cw_serde, QueryResponses};
+use cosmwasm_std::Addr;
+use cw20::Cw20ReceiveMsg;
+
+#[cw_serde]
+pub struct InstantiateMsg {
+    pub token_symbol: String,
+    pub token_contract_address: Addr,
+}
+
+#[cw_serde]
+pub enum ExecuteMsg {
+    Receive(Cw20ReceiveMsg),
+}
+
+#[cw_serde]
+pub enum ReceiveMsg {
+    AnExecuteMsg {}
+}
+
+#[cw_serde]
+#[derive(QueryResponses)]
+pub enum QueryMsg {}
